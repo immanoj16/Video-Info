a3954401964fc245004f7b7a800f437d272bb59a
immanoj16 <mmaharana14@gmail.com>
2018-02-01 12:27:17 +0530

    Add: file

--- main.js
+++ main.js
@@ -4,13 +4,15 @@ const url = require('url')
 
 let win
 function createWindow() {
-  win = new BrowserWindow({})
+  win = new BrowserWindow({width: 800, height: 600})
 
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
+
+  win.webContents.openDevTools()
 }
 
 app.on('ready', createWindow)
